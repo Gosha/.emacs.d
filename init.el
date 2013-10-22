31cf9c9de7a3bcdd9f958a9b6c875dac4df87d7e
Georgij Zacharov <gosha.za@gmail.com>
2013-10-22 04:40:50 +0200

    Fix no start screen. No toolbar. Backups in own directory. Backup VC files. Save places

--- init.el
+++ init.el
@@ -3,4 +3,20 @@
 (load custom-file 'noerror)
 
 ;; Don't show the silly start screen
-(inhibit-startup-screen t)
+(setq inhibit-startup-screen t)
+
+;; No Tool bar, but I kind of like the other bars
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
