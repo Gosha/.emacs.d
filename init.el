c968db0f63d450811ee018729ee856702ec3893f
Georgij Zacharov <gosha.za@gmail.com>
2014-10-14 00:30:44 +0200

    eclim for eclipse

--- init.el
+++ init.el
@@ -179,6 +179,13 @@
          :after (progn
                   (setq wg-prefix-key (kbd "C-c w"))
                   (workgroups-mode 1)))
+        (:name eclim
+         :after (progn
+        ;; TODO: make it load only when company is loaded
+                  (add-hook 'eclim-mode-hook
+                            (lambda ()
+                              (require 'company-emacs-eclim)
+                              (company-emacs-eclim-setup)))))
 	))
 
 (setq my-packages
